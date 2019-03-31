bd123effe42eab63810d39d1f26c0f4760c659e6
Tanuka Das <das.tanuka@yahoo.com>
2019-03-31 23:56:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,9 +70,9 @@ function placeOrder(cardNumber) {
     let totalValue = 0;
     totalValue = total();
     cart = [];
-  return "Your total cost is " + "$" + total() + ", " + "which will be charged to the card " + cardNumber + "."
+  return ("Your total cost is " + "$" + total() + ", " + "which will be charged to the card " + cardNumber + ".")
   } else {
-    return "Sorry, we don't have a credit card on file for you."
+    return ("Sorry, we don't have a credit card on file for you.")
   }
 
 
