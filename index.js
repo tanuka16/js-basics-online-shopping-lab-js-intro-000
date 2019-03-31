4dcfd9beb99454bdd85f8509aea8bfa119a9bf54
Tanuka Das <das.tanuka@yahoo.com>
2019-03-31 23:30:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,9 +67,9 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber){
-    let total1 = total;
+    let totalValue = total;
     cart = [];
-  return "Your total cost is " + total1 + ", " + "which will be charged to the card " + cardNumber + "."
+  return "Your total cost is " + totalValue + ", " + "which will be charged to the card " + cardNumber + "."
   } else {
     return "Sorry, we don't have a credit card on file for you."
 }
