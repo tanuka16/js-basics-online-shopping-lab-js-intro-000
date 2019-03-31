463ff4c2814982cfd5f9d7d7b506e6c07ad43cfb
Tanuka Das <das.tanuka@yahoo.com>
2019-03-31 23:58:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function placeOrder(cardNumber) {
   totalValue = total();
   cart = [];
 
-  if (cardNumber){
+  if (cardNumber){                        //
         return ("Your total cost is " + "$" + total() + ", " + "which will be charged to the card " + cardNumber + ".")
   } else {
     return ("Sorry, we don't have a credit card on file for you.")
