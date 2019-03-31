5eb40102e67e3eb6720c7a2b00c365c409d72f6e
Tanuka Das <das.tanuka@yahoo.com>
2019-03-31 22:32:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,10 +71,10 @@ function placeOrder(cardNumber) {
 
   if (cardNumber===undefined || (cardNumber===null || cardNumber.length <= 0)
   {
-    return "Sorry, we don't have a credit card on file for you.";
+    console.log("Sorry, we don't have a credit card on file for you.");
   }
   else {
     console.log(`Your total cost is \$${totalValue}, which will be charged to the card ${cardNumber}.`)
   }
-  
+
 }
